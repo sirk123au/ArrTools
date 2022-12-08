3b170470a46fa89f9a3989771e3759f2336e192c
Kris <sirk123au@gmail.com>
2022-12-08 16:24:32 +1000

    Reworked Te Rework

--- radarr_add_from_list.py
+++ radarr_add_from_list.py
@@ -66,10 +66,10 @@ log = logging.getLogger("app." + __name__)
 
 ########################################################################################################################
 
-def add_movie(title, year, imdbid):
+def add_movie(title, year, imdbid, quality_profile_id):
+
     global movie_added_count
     global movie_exist_count
-    global ProfileId
 
     if year == "" or year is None:
         year = get_year(title)
@@ -79,21 +79,17 @@ def add_movie(title, year, imdbid):
     # Store Radarr Server imdbid for faster matching
     current_movie_ids = [movie.get('imdbId') for movie in RadarrData]
 
-    if quality_profile_id is None or quality_profile_id == '':
-        log.warning("Quality profile not set in config file.")
-        ProfileId = select_profile_id()
-    elif match_profile_id(quality_profile_id):
-        ProfileId = quality_profile_id
+    if match_profile_id(quality_profile_id): ProfileId = get_profile_from_name(quality_profile_id)
 
     if imdbid is None:
-        log.info(f"Cannot find IMDbId for {title}, attempting to add without it...")
+        log.warning(f"Cannot find IMDbId for {title}, attempting to add without it...")
         # Search by Movie Title in Radarr
         headers = {"Content-type": "application/json", 'Accept': 'application/json'}
         url = f"{baseurl}{urlbase}/api/v3/movie/lookup?term={title.replace(' ', '%20')}&apikey={api_key}"
         radarr_api_response = requests.get(url, headers=headers)
         data = json.loads(radarr_api_response.text)
         if radarr_api_response.text == "[]":
-            log.error(f"\u001b[35mSorry. We couldn't find any movies matching {title} ({year})\u001b[0m")
+            log.error(f"Sorry. We couldn't find any movies matching {title} ({year})")
             return
         if radarr_api_response.status_code == 200:
             tmdbid = data[0]["tmdbId"]
@@ -107,7 +103,7 @@ def add_movie(title, year, imdbid):
                 "year":                year,
                 "tmdbId":              tmdbid,
                 "titleslug":           titleslug,
-                "monitored":           'true',
+                "monitored":           True,
                 "minimumAvailability": "released",
                 "rootFolderPath":      rootfolderpath,
                 "images":              images,
@@ -120,26 +116,20 @@ def add_movie(title, year, imdbid):
             log.debug(f"Radarr response: {radarr_api_response.status_code}: {radarr_api_response.text}")
             if radarr_api_response.status_code == 201:
                 movie_added_count += 1
-                # TODO: Remove unicode characters for colours, replace with a module
                 if searchForMovie:
-                    log.info(
-                        f"\u001b[36mtm{tmdbid}\t         \u001b[0m{title} ({year}) \u001b[32mAdded to Radarr :) "
-                        f"\u001b[36;1mNowSearching.\u001b[0m\n")
+                    log.info(f"{tmdbid}\t {title} ({year}) Added to Radarr :) NowSearching.")
                 else:
-                    log.info(
-                        f"\u001b[36mtm{tmdbid}\t         \u001b[0m{title} ({year}) \u001b[32mAdded to Radarr :) "
-                        f"\u001b[31mSearch Disabled.\u001b[0m\n")
+                    log.info(f"{tmdbid}\t {title} ({year}) Added to Radarr :) Search Disabled.")
             elif radarr_api_response.status_code == 400:
                 movie_exist_count += 1
-                log.info(
-                    f"\u001b[36mtm{tmdbid}\t         \u001b[0m{title} ({year}) already Exists in Radarr.\n")
+                log.info(f"{tmdbid}\t {title} ({year}) already Exists in Radarr.")
                 return
         else:
-            log.error(f"\u001b[35m{imdbid}\t {title} ({year}) Not found, Not added to Radarr.\u001b[0m\n")
+            log.error(f"{imdbid}\t {title} ({year}) Not found, Not added to Radarr.")
             return
 
     elif imdbid not in current_movie_ids:
-        log.info(f"Found IMDbId for {title} - attempting to add to Radarr...")
+        log.warning(f"Found IMDbId for {title} - attempting to add to Radarr...")
         # Build json Data to import into radarr
         session = requests.Session()
         adapter = requests.adapters.HTTPAdapter(max_retries=20)
@@ -150,7 +140,7 @@ def add_movie(title, year, imdbid):
         url = f"{baseurl}{urlbase}/api/v3/movie/lookup/imdb?imdbId={imdbid}&apikey={api_key}"
         radarr_api_response = session.get(url, headers=headers)
         if len(radarr_api_response.text) == 0:
-            log.error(f"\u001b[35mSorry. We couldn't find any movies matching {title} ({year})\u001b[0m")
+            log.error(f"Sorry. We couldn't find any movies matching {title} ({year})")
             return
         if radarr_api_response.status_code == 200:
             data = json.loads(radarr_api_response.text)
@@ -171,11 +161,10 @@ def add_movie(title, year, imdbid):
                 "images":              images,
                 "addOptions":          {"searchForMovie": searchForMovie}})
         elif radarr_api_response.status_code == 404:
-            log.error(f"\u001b[36m{imdbid}\t \u001b[0m{title} ({year}) Movie not found... unable to add to Radarr\n")
+            log.error(f"{imdbid}\t {title} ({year}) Movie not found... unable to add to Radarr")
             return
         elif radarr_api_response.status_code == 500:
-            log.error(f"\u001b[36m{imdbid}\t \u001b[0m{title} ({year}) Can't find TMDB ID - movie may have been "
-                      f"removed!\n")
+            log.error(f"{imdbid}\t {title} ({year}) Can't find TMDB ID - movie may have been removed!")
             return
         else:
             log.error("Something else has happened.")
@@ -190,16 +179,28 @@ def add_movie(title, year, imdbid):
             movie_added_count += 1
             if searchForMovie:  # Check If you want to force download search
                 log.info(
-                    f"\u001b[36m{imdbid}\t \u001b[0m{title} ({year}) \u001b[32mAdded to Radarr :) \u001b[36;1mNow "
-                    f"Searching.\u001b[0m\n")
+                    f"{imdbid}\t {title} ({year}) Added to Radarr :) Now Searching.")
             else:
                 log.info(
-                    f"\u001b[36m{imdbid}\t \u001b[0m{title} ({year}) \u001b[32mAdded to Radarr :) \u001b[31mSearch "
-                    f"Disabled.\u001b[0m\n")
+                    f"{imdbid}\t {title} ({year}) Added to Radarr :) Search Disabled.")
     else:
         movie_exist_count += 1
-        log.info(f"\u001b[36m{imdbid}\t \u001b[0m{title} ({year}) already exists in Radarr!")
+        log.info(f"{imdbid}\t {title} ({year}) already exists in Radarr!")
         return
+
+def get_profile_from_name(name :str):
+    """
+Converts Profile Name -> ID
+    :return: ID
+    :type: any
+    """  
+    if name.isdigit(): return name
+    profiles = quality_profiles
+    profile = next((item for item in profiles if item["name"] == name), False)
+    if not profile:
+        log.error(f"Could not find profile_id for instance profile ID {name}")
+        return select_profile_id()
+    return  profile.get('id')
 
 
 def get_quality_profiles() -> list:
@@ -207,9 +208,9 @@ def get_quality_profiles() -> list:
 Parses local Radarr API to get the server's quality profiles and return them to calling
 functions
     :return: The server's quality profiles as a list
-    :rtype: list
+    :type: list
     """
-    #log.info("Getting quality profiles...")
+    log.info("Getting quality profiles...")
     headers = {"Content-type": "application/json", "X-Api-Key": f"{api_key}"}
     url = f"{baseurl}{urlbase}/api/v3/qualityProfile"
     r = requests.get(url, headers=headers)
@@ -217,17 +218,15 @@ functions
     return profiles_json
 
 
-def select_profile_id() -> int:
+def select_profile_id():
     """
 Allows the user to select a quality profile ID at runtime using console input
     :return: Returns an integer that matches the specified quality profile Id
-    :rtype: int
+    :type: int
 
     """
     selected = False
     profile_choice = -1
-    # TODO: Find a way to make this call only happen once per run
-    quality_profiles = get_quality_profiles()
     print("\nPlease enter a valid profile ID:")
     for profile in quality_profiles:
         print(f"{profile.get('id')}: {profile.get('name')}")
@@ -235,25 +234,25 @@ Allows the user to select a quality profile ID at runtime using console input
         user_input = input("> ")
         selected = match_profile_id(user_input)
         if selected:
-            profile_choice = int(user_input)
+            profile_choice = user_input
     return profile_choice
 
 
-def match_profile_id(quality_id: str) -> bool:
+def match_profile_id(quality_id) -> bool:
     """
 Checks if a given quality profile ID matches with one from the API.
-    :rtype: bool
-    :param quality_id: Quality profile ID as a string
+    :type: bool
+    :param quality_id: Quality profile ID as a string of int
     :return: Returns true if the quality profile is matched/found
     """
-    profiles = get_quality_profiles()
-    if type(quality_id) == int or type(quality_id) == float:
-        profile = next((p for p in profiles if p["id"] == int(quality_id)), False)
-    else:
+    profiles = quality_profiles
+    if not quality_id.isdigit():
         profile = next((p for p in profiles if p["name"] == quality_id), False)
+    else:
+        profile = next((p for p in profiles if p['id'] == int(quality_id)), False)
     if not profile:
         log.error(f'Could not find profile_id for instance profile ID: {quality_id}')
-        sys.exit(0)
+        if match_profile_id(select_profile_id()): return True
     return True
 
 
@@ -305,6 +304,8 @@ Uses OMDB to check the year that a given movie released
 
 def main():
     global RadarrData
+    global quality_profiles 
+    global quality_profile_id
 
     print('\033c')
     if sys.version_info[0] < 3:
@@ -323,6 +324,7 @@ def main():
     rsp = requests.get(url, headers=headers)
     if rsp.status_code == 200:
         RadarrData = json.loads(rsp.text)
+        quality_profiles = get_quality_profiles()
     else:
         log.error("Failed to connect to Radarr...")
 
@@ -343,7 +345,10 @@ def main():
             year = row['year']
             imdbid = row['imdbid']
             try:
-                add_movie(title, year, imdbid)
+                if quality_profile_id is None or quality_profile_id == '':
+                    log.warning("Quality profile not set in config file.")
+                    quality_profile_id = select_profile_id()
+                add_movie(title, year, imdbid,quality_profile_id)
             except Exception as e:
                 exc_type, exc_obj, exc_tb = sys.exc_info()
                 fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
